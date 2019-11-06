90be56cb0dea010baccd5ba206317fcd6858d5ed
hammermicro <56226926+hammermicro@users.noreply.github.com>
2019-11-05 19:32:32 -0800

    updated HW code

--- HW5.R
+++ HW5.R
@@ -9,22 +9,22 @@ qplot(x=IQquartile, y=logincome, data=aov_data, geom=c("boxplot"))
 log_income_summary = summary(aov_data$logincome)
 
 log_data_aov = aov(logincome~IQquartile, data=aov_data)
+print(log_data_aov)
 log_data_anova = anova(log_data_aov)
-
+print(log_data_anova)
 plot(log_data_aov,which=1)
-
 quartile_means <- with(aov_data,unlist(lapply(split(logincome,IQquartile),mean)))
 print(quartile_means)
 t_df = qt(0.975, 2580)
 print(t_df)
 num_participants_in_quartile <- with(aov_data,unlist(lapply(split(logincome,IQquartile),length)))
 print(num_participants_in_quartile)
-conf_int_plus = (10.785 - 10.050) + (1.961)*(sqrt(0.937))*(sqrt((2)/1292))
+conf_int_plus = (10.050-10.785) + (t_df)*((0.9369))*(sqrt((1/646)+(1/646)))
 print(conf_int_plus)
-conf_int_minus = (10.785 - 10.050) - (1.961)*(sqrt(0.937))*(sqrt((2)/1292))
+conf_int_minus = (10.785 - 10.050) - (t_df)*((0.9369))*(sqrt((1/646)+(1/646)))
 print(conf_int_minus)
 pt_estimate = exp(10.785-10.050)
 print(pt_estimate)
-print(exp(c(0.66, 0.81)))
+print(exp(c(conf_int_plus, conf_int_minus)))
 
 
